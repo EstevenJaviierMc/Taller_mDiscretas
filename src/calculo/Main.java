c8c60ec2701864d43f39cae4f19a2d0f63b1c2a4
GOMEZ AGUILAR <GOMEZ AGUILAR 2@LAPTOP-UKP96FC0>
2019-10-03 14:02:44 -0500

    Ready 100%

--- src/calculo/Main.java
+++ src/calculo/Main.java
@@ -2,63 +2,23 @@ package calculo;
 
 import java.util.Scanner;
 
-/**
- *
- * @author GOMEZ AGUILAR 2
- */
 public class Main {
-
-    public static double Perimetro(double P) {
-        double Perimetro = (Math.sqrt(2) * P) / 2;
-        return Perimetro;
-    }
-
-    public static double Area(double A) {
-        double Area = A / 2;
-        return Area;
-    }
-
-    public static double Lado(double L) {
-        double Lado = (L / 2) * Math.sqrt(2);
-        return Lado;
-    }
-
+    
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
 
         System.out.print("Digite la medida del lado del cuadrado exterior: ");
         double L = leer.nextDouble();
-
-        double A = L * L;
-        double P = 4 * L;
-
-//        System.out.println("Area y perimetro respectivamente: " + A + " - " + P);
-        System.out.println("");
-
-        double totalP = 0;
-        double totalA = 0;
-
-        for (int i = 0; i < 90; i++) {
-            //Area de los cuadros
-            System.out.println(A + " _");
-            totalA = totalA + A;
-            A = Area(A);
-            
-        }
         
-        System.out.println("");
-        
-        for (int i = 0; i < 97687; i++) {
-            //Perimetro de los cuadros
-            System.out.println(P + " _");
-            totalP = totalP + P;
-            P = Perimetro(P);
-        }
-        System.out.println(totalP);
+        //               4( L / (1-(1/√2)) )
+        double totalP = 4 * (L / (1 - (1 / Math.sqrt(2))));    
+
+        //               2L^2
+        double totalA = 2 * (L * L);
 
         System.out.println("");
         System.out.println("La suma del área de todos los cuadros es: " + totalA);
-        System.out.println("La suma de todos perímetros del cuadrado es: " + totalP);
+        System.out.println("La suma de todos los perímetros del cuadrado es: " + totalP);
     }
 
 }
