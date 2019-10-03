2eca3bf5d073647634fe4a66efc5516b2468ee09
GOMEZ AGUILAR <GOMEZ AGUILAR 2@LAPTOP-UKP96FC0>
2019-10-03 15:09:34 -0500

    Ready 100% con vista

--- src/calculo/Main.java
+++ src/calculo/Main.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     
-    public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-
-        System.out.print("Digite la medida del lado del cuadrado exterior: ");
-        double L = leer.nextDouble();
-        
-        //               4( L / (1-(1/√2)) )
-        double totalP = 4 * (L / (1 - (1 / Math.sqrt(2))));    
-
-        //               2L^2
-        double totalA = 2 * (L * L);
-
-        System.out.println("");
-        System.out.println("La suma del área de todos los cuadros es: " + totalA);
-        System.out.println("La suma de todos los perímetros del cuadrado es: " + totalP);
-    }
+//    public static void main(String[] args) {
+//        Scanner leer = new Scanner(System.in);
+//
+//        System.out.print("Digite la medida del lado del cuadrado exterior: ");
+//        double L = leer.nextDouble();
+//        
+//        //               4( L / (1-(1/√2)) )
+//        double totalP = 4 * (L / (1 - (1 / Math.sqrt(2))));    
+//
+//        //               2L^2
+//        double totalA = 2 * (L * L);
+//
+//        System.out.println("");
+//        System.out.println("La suma del área de todos los cuadros es: " + totalA);
+//        System.out.println("La suma de todos los perímetros del cuadrado es: " + totalP);
+//    }
 
 }
